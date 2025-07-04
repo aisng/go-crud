6694dc4c515527678c1486d530032f33d195a88c
aisng <tasignas@gmail.com>
2025-07-04 15:06:05 +0300

    test: UserHandler.GetByID

--- internal/handler/user_test.go
+++ internal/handler/user_test.go
@@ -1,14 +1,12 @@
 package handler
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"go-crud/internal/domain"
 	"io"
 	"net/http"
 	"net/http/httptest"
-	"reflect"
 	"strings"
 	"testing"
 	"time"
@@ -21,7 +19,7 @@ func TestUserHandler_Create(t *testing.T) {
 		name       string
 		method     string
 		body       string
-		repoErr    error
+		wantErr    error
 		wantStatus int
 		wantBody   string
 	}{
@@ -29,7 +27,7 @@ func TestUserHandler_Create(t *testing.T) {
 			name:       "success",
 			method:     http.MethodPost,
 			body:       `{"username": "test", "email": "test@email.net", "password": "mypw123"}`,
-			repoErr:    nil,
+			wantErr:    nil,
 			wantStatus: http.StatusCreated,
 			wantBody: fmt.Sprintf(`{"id":1,"email":"test@email.net","username":"test","created_at":"%s","updated_at":"%s"}`,
 				fixedTime.Format(time.RFC3339), fixedTime.Format(time.RFC3339)),
@@ -38,7 +36,7 @@ func TestUserHandler_Create(t *testing.T) {
 			name:       "invalid method",
 			method:     http.MethodGet,
 			body:       "",
-			repoErr:    nil,
+			wantErr:    nil,
 			wantStatus: http.StatusMethodNotAllowed,
 			wantBody:   "Method not allowed\n",
 		},
@@ -46,7 +44,7 @@ func TestUserHandler_Create(t *testing.T) {
 			name:       "invalid json",
 			method:     http.MethodPost,
 			body:       `{bad json}`,
-			repoErr:    nil,
+			wantErr:    nil,
 			wantStatus: http.StatusBadRequest,
 			wantBody:   "Invalid JSON\n",
 		},
@@ -54,7 +52,7 @@ func TestUserHandler_Create(t *testing.T) {
 			name:       "repository error",
 			method:     http.MethodPost,
 			body:       `{"username": "test", "email": "test@email.net"}`,
-			repoErr:    errors.New("repo error"),
+			wantErr:    errors.New("repo error"),
 			wantStatus: http.StatusInternalServerError,
 			wantBody:   "Failed to create user: repo error\n",
 		},
@@ -64,7 +62,7 @@ func TestUserHandler_Create(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			repo := &mockUserRepo{
 				createFunc: func(*domain.User) error {
-					return test.repoErr
+					return test.wantErr
 				},
 			}
 
@@ -83,14 +81,112 @@ func TestUserHandler_Create(t *testing.T) {
 				t.Errorf("expected status code: %v, got: %v", test.wantStatus, resp.StatusCode)
 			}
 
-			respBody, _ := io.ReadAll(resp.Body)
+			respBody, err := io.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("failed to read response body: %v", err)
+			}
 			respBodyStr := string(respBody)
 
-			var got, want map[string]any
-			json.Unmarshal([]byte(respBodyStr), &got)
-			json.Unmarshal([]byte(test.wantBody), &want)
-			if !reflect.DeepEqual(got, want) {
-				t.Errorf("expected response: %v, got: %v", want, got)
+			if strings.TrimSpace(string(respBody)) != strings.TrimSpace(test.wantBody) {
+				t.Errorf("expected response body: %s, got: %s", test.wantBody, respBodyStr)
+			}
+		})
+	}
+}
+
+func TestUserHandler_GetByID(t *testing.T) {
+	tests := []struct {
+		name       string
+		method     string
+		path       string
+		repoUser   *domain.User
+		wantErr    error
+		wantStatus int
+		wantBody   string
+	}{
+		{
+			name:   "success",
+			method: http.MethodGet,
+			path:   "/users/1",
+			repoUser: &domain.User{
+				ID:        1,
+				Email:     "test@email.com",
+				Username:  "testuser",
+				CreatedAt: fixedTime,
+				UpdatedAt: fixedTime,
+			},
+			wantErr:    nil,
+			wantStatus: http.StatusOK,
+			wantBody: fmt.Sprintf(`{"id":1,"email":"test@email.com","username":"testuser","created_at":"%s","updated_at":"%s"}`,
+				fixedTime.Format(time.RFC3339), fixedTime.Format(time.RFC3339)),
+		},
+		{
+			name:       "user not found",
+			method:     http.MethodGet,
+			path:       "/users/9999",
+			repoUser:   nil,
+			wantErr:    fmt.Errorf("user not found"),
+			wantStatus: http.StatusNotFound,
+			wantBody:   "User not found\n",
+		},
+		{
+			name:       "method not allowed",
+			method:     http.MethodPost,
+			path:       "/users/1",
+			repoUser:   nil,
+			wantErr:    nil,
+			wantStatus: http.StatusMethodNotAllowed,
+			wantBody:   "Method not allowed\n",
+		},
+		{
+			name:       "invalid path",
+			method:     http.MethodGet,
+			path:       "/invalid/path/1",
+			repoUser:   nil,
+			wantErr:    fmt.Errorf("invalid path format"),
+			wantStatus: http.StatusBadRequest,
+			wantBody:   "invalid path format\n",
+		},
+		{
+			name:       "invalid id",
+			method:     http.MethodGet,
+			path:       "/users/1b",
+			repoUser:   nil,
+			wantErr:    fmt.Errorf("Invalid paramter 'id'\n"),
+			wantStatus: http.StatusBadRequest,
+			wantBody:   "Invalid paramter 'id'\n",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			repo := &mockUserRepo{
+				getByIDFunc: func(id int64) (*domain.User, error) {
+					return test.repoUser, test.wantErr
+				},
+			}
+			handler := NewUserHandler(repo)
+
+			req := httptest.NewRequest(test.method, test.path, nil)
+			w := httptest.NewRecorder()
+			handler.GetByID(w, req)
+
+			resp := w.Result()
+
+			defer resp.Body.Close()
+
+			if test.wantStatus != resp.StatusCode {
+				t.Errorf("expected status code: %v, got: %v", test.wantStatus, resp.StatusCode)
+			}
+
+			respBody, err := io.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("failed to read response body: %v", err)
+			}
+			respBodyStr := string(respBody)
+
+			if strings.TrimSpace(string(respBody)) != strings.TrimSpace(test.wantBody) {
+				t.Errorf("expected response body: %s, got: %s", test.wantBody, respBodyStr)
 			}
 		})
 	}
