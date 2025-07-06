105dee3ad9f061c1710a51551d7a08eb123524bc
aisng <tasignas@gmail.com>
2025-07-06 16:11:06 +0300

    style: replace interface{} w/ any

--- internal/handler/response.go
+++ internal/handler/response.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func WriteResponse(w http.ResponseWriter, data interface{}, statusCode int) {
+func WriteResponse(w http.ResponseWriter, data any, statusCode int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(data)
